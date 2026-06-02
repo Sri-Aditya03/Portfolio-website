9f215d50c1870d99079dee08a88a51867705d4f7
Sri-Aditya03 <srivastava03adi@gmail.com>
2026-06-02 23:23:10 +0530

    Updated home page and about me page

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -7,25 +7,25 @@ page_router = APIRouter()
 
 @page_router.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="home.html", context={"request": request})
 
 @page_router.get("/about")
 def about(request: Request):
-    return templates.TemplateResponse("about.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="about.html", context={"request": request})
 
 @page_router.get("/experience")
 def experience(request: Request):
-    return templates.TemplateResponse("experience.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="experience.html", context={"request": request})
 
 @page_router.get("/projects")
 def projects(request: Request):
-    return templates.TemplateResponse("projects.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="projects.html", context={"request": request})
 
 @page_router.get("/contact")
 def contact(request: Request):
-    return templates.TemplateResponse("contact.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="contact.html", context={"request": request})
 
 @page_router.get("/resume")
 def resume(request: Request):
-    return templates.TemplateResponse("resume.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="resume.html", context={"request": request})
 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 from app.routes.pages import page_router
 from app.routes.api import api_router
@@ -8,8 +7,6 @@ from app.routes.api import api_router
 app = FastAPI(title="Aditya Portfolio")
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-templates = Jinja2Templates(directory="app/templates")
 
 # Routers
 app.include_router(page_router)
